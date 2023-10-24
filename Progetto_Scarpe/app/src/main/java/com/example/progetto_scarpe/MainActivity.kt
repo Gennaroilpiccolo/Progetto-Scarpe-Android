075302b0dee766921cafcd5c77bae8d5796056a9
Gennaroilpiccolo <126774078+Gennaroilpiccolo@users.noreply.github.com>
2023-10-24 14:22:21 +0200

    aggiornamento

--- Progetto_Scarpe/app/src/main/java/com/example/progetto_scarpe/MainActivity.kt
+++ Progetto_Scarpe/app/src/main/java/com/example/progetto_scarpe/MainActivity.kt
@@ -2,10 +2,40 @@ package com.example.progetto_scarpe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.fragment.app.Fragment
+import com.example.progetto_scarpe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.Login -> {
+
+                    supportFragmentManager.beginTransaction().apply {
+                        replace(R.id.fragmentContainer, RegisterFragment())
+                        commit()
+                    }
+                    // Gestisci il clic sul pulsante 1 (Item 1)
+                    // Esegui l'azione desiderata
+                    true
+                }
+                R.id.Search -> {
+                    // Gestisci il clic sul pulsante 2 (Item 2)
+                    // Esegui l'azione desiderata
+                    true
+                }
+                // Aggiungi ulteriori casi per altri pulsanti, se necessario
+                else -> false
+            }
+        }
+
+
     }
 }
